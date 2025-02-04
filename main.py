4a40722a890ec1798607e86ad86afb3fa5ad7c78
Scinawa <2940017+Scinawa@users.noreply.github.com>
2025-02-04 23:48:00 +0800

    Make coding great again

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import configparser
 from mlflow.tracking import MlflowClient
+import mlflow
+from mlflow.entities import Metric
+from mlflow.tracking import MlflowClient
+import pprint
 
 from data_structure import data_template
-from utils import generate_latex_table, update_data_template, compute_average_metrics
+from utils import (
+    generate_latex_table,
+    update_data_template,
+    compute_average_metrics_of_a_step,
+    score_step,
+    map_metrics_to_template,
+)
 
 
 def load_config(config_file):
@@ -22,11 +32,16 @@ if __name__ == "__main__":
     config_file = "config.ini"
     config = load_config(config_file)
 
-    #print(config.sections())
+    # print(config.sections())
 
-    average_metrics = {}
-    # import pdb
-    # pdb.set_trace()
+    average_best_metrics = {}
+    simplified_best_metrics = {}
+    import pdb
+
+    best_steps = {}
+
+    mlflow.set_tracking_uri(config["DEFAULT"]["mlflow_data_path"])
+    client = MlflowClient()
 
     for section in config.sections():
         if section == "DEFAULT":
@@ -34,22 +49,62 @@ if __name__ == "__main__":
 
         column_id = int(section.split("-")[-1])
 
-        try:        
-            average_metrics[column_id] = compute_average_metrics(
-                config[section]['experiment_id'],
-                config[section]['run_id'],
-                int(config[section]['step_number']),
-                None,
-                config['DEFAULT']['mlflow_data_path'],
+        best_step = None
+        best_score = float("-inf")
+
+        # Validate the run belongs to the experiment
+        run = client.get_run(config[section]["run_id"])
+
+        step_numbers = set()
+
+        # find all steps so we can iterate all the steps later
+        for metric in run.data.metrics.keys():
+            metric_history = client.get_metric_history(run.info.run_id, metric)
+            for metric_data in metric_history:
+                step_numbers.add(metric_data.step)
+
+        # find the average metric for each step
+        # and in the meanwhile find the best step according to the metric.
+        for step in sorted(step_numbers):
+            try:
+                average_metrics_per_step = compute_average_metrics_of_a_step(
+                    client, run, step
+                )
+                # print("Average metrics per step:", average_metrics_per_step)
+
+            except Exception as e:
+                print(
+                    f"Failed to compute average metric in {step} in {section}. \n Reason: {e}"
+                )
+
+            temporary_dictionary_of_metric_at_step = map_metrics_to_template(
+                average_metrics_per_step
             )
-        except Exception as e:
-            print(f"Failed to compute average metrics for {section}. \n Reason: {e}")
-            average_metrics[column_id] = {}
 
-    print("Average Metrics:", average_metrics)
+            # compute score of the step....
+            # weights is just the unit vector for now
+            score = score_step(temporary_dictionary_of_metric_at_step)
+            if score > best_score:
+                best_score = score
+                best_step = step
+                average_best_metrics[section] = average_metrics_per_step
+                simplified_best_metrics[section] = (
+                    temporary_dictionary_of_metric_at_step
+                )
 
-    data = update_data_template(data_template, average_metrics)
+        best_steps[section] = best_step
 
+        print("Best steps:", best_steps)
+
+    # pdb.set_trace()
+    # Update the data template with the average metrics
+    print("Average best metrics:")
+    pprint.pprint(average_best_metrics)
+
+    print("Average simplified best metrics:")
+    pprint.pprint(simplified_best_metrics)
+
+    data = update_data_template(data_template, average_best_metrics)
 
     # Generate the table
     latex_code = generate_latex_table(data)
--- utils.py
+++ utils.py
@@ -1,29 +1,71 @@
 import mlflow
 from mlflow.entities import Metric
 from mlflow.tracking import MlflowClient
+import numpy as np
+
+
+def score_step(average_metrics, weights=None):
+    if weights is None:
+        weights = {key: 1 for key in average_metrics}
+
+    if average_metrics.keys() != weights.keys():
+        raise ValueError("The keys of metrics and weights must be the same.")
+
+    score = sum(average_metrics[key] * weights[key] for key in average_metrics)
+    return score
+
+
+def map_metrics_to_template(metrics):
+    # Mapping of metric names to the desired keys
+    metric_mapping = {
+        "val/fake_np": "n mol",
+        "val/fake_qed": "QED",
+        "val/fake_logp": "Solub.",
+        "val/fake_sas": "SA",
+        "val/mean_of_prods": "KL",
+    }
+
+    # Create the new dictionary with the mapped values
+    mapped_metrics = {}
+    for metric_name, metric_value in metrics.items():
+        if metric_name in metric_mapping:
+            mapped_metrics[metric_mapping[metric_name]] = metric_value
+
+    return mapped_metrics
 
 
 def update_data_template(data_template, average_metrics):
     # Mapping of average metric names to data template keys
     metric_mapping = {
-        'Validation_fake_data_nmol': 'n mol',
-        'Validation_fake_data_logp': 'Solub.',
-        'Validation_fake_data_sas': 'SA',
-        'Validation_fake_data_qed': 'QED'
+        "val/fake_np": "n mol",
+        "val/fake_qed": "QED",
+        "val/fake_logp": "Solub.",
+        "val/fake_sas": "SA",
+        # "val/mean_of_prods": "KL",
     }
 
-    # Iterate over the z values (2, 3, 4)
-    for z in [2, 3, 4]:
-        # Get the average metrics for the current z value
-        metrics = average_metrics.get(z, {})
+    for base in ["As-moles", "qAs-moles"]:
 
-        # Update the data template with the average metrics
-        for metric_name, metric_value in metrics.items():
-            if metric_name in metric_mapping:
-                data_template[z]['new']['As-moles'][metric_mapping[metric_name]] = metric_value
-                data_template[z]['new']['Qas-Moles'][metric_mapping[metric_name]] = metric_value
+        # Iterate over the z values (2, 3, 4)
+        for z in [2, 3, 4]:
+            # Construct the key for the current z value
+            key = f"{base}-{z}"
+
+            # Get the average metrics for the current key
+            metrics = average_metrics.get(key, {})
+
+            # Update the data template with the average metrics
+            for metric_name, metric_value in metrics.items():
+                if metric_name in metric_mapping:
+                    data_template[z]["new"]["As-moles"][
+                        metric_mapping[metric_name]
+                    ] = metric_value
+                    data_template[z]["new"]["Qas-Moles"][
+                        metric_mapping[metric_name]
+                    ] = metric_value
 
     return data_template
+
 
 def generate_latex_table(data):
     rows = ["n mol", "QED", "Solub.", "SA", "KL"]  # Row names
@@ -33,12 +75,13 @@ def generate_latex_table(data):
 
     # Start building the LaTeX table
     latex_table = r"""
-    \begin{table}[]
-    \begin{tabular}{ccccccc|cccccc|cccccc}
+    \begin{table*}[]
+    \resizebox{\columnwidth}{!}{
+       \begin{tabular}{ccccccc|cccccc|cccccc}
         & \multicolumn{6}{c|}{z = 2}                                                       & \multicolumn{6}{c|}{z = 3}                                                       & \multicolumn{6}{c}{z = 4}                                                        \\ \cline{2-19} 
-        & QmolGan & MolGan & \multicolumn{1}{c|}{p-value} & As-moles & Qas-Moles & p-value & QmolGan & MolGan & \multicolumn{1}{c|}{p-value} & As-moles & Qas-Moles & p-value & QmolGan & MolGan & \multicolumn{1}{c|}{p-value} & As-moles & Qas-Moles & p-value \\
+        & QMG & MG & \multicolumn{1}{c|}{p-value} & ASM & QSM & p-value & QMG & MG & \multicolumn{1}{c|}{p-value} & ASM & QSM & p-value & QMG & MG & \multicolumn{1}{c|}{p-value} & ASM & QSM & p-value \\
     """
-    
+
     # Fill rows with data
     for row_name in rows:
         latex_table += f"{row_name} & "  # Row label
@@ -47,77 +90,102 @@ def generate_latex_table(data):
                 headers = old_headers if part == "old" else new_headers
                 for header in headers:
                     # Fetch the data or leave blank if missing
-                    value = data.get(z, {}).get(part, {}).get(header, {}).get(row_name, "")
+                    value = (
+                        data.get(z, {}).get(part, {}).get(header, {}).get(row_name, "")
+                    )
 
                     if isinstance(value, float):
                         value = f"{value:.3f}"
 
                     latex_table += f"\({value}\) & "
         latex_table = latex_table.rstrip("& ") + r" \\" + "\n"  # End the row
-    
+
     # Close the LaTeX table
     latex_table += r"""
-    \end{tabular}
-    \end{table}
+    \end{tabular}}
+    \end{table*}
+    
     """
     return latex_table
 
 
-def compute_average_metrics(experiment_id, run_id, step_number, metric_names, mlflow_data_path):
+def compute_average_metrics_of_a_step(client, run, step_number):
     """
     Computes the average of specified metrics for a given experiment ID, run ID, and step number.
-    
-    Args:
-        experiment_id (str): The MLflow experiment ID.
-        run_id (str): The MLflow run ID.
-        step_number (int): The step number to filter by.
-        metric_names (list): A list of metric names to compute the average for.
-    
+
     Returns:
         dict: A dictionary where keys are metric names and values are their averages for the given step.
     """
-    mlflow.set_tracking_uri(mlflow_data_path)
+    # mlflow.set_tracking_uri(mlflow_data_path)
 
-    client = MlflowClient()
-    
-    # Validate the run belongs to the experiment
-    run = client.get_run(run_id)
-    if run.info.experiment_id != experiment_id:
-        raise ValueError("The specified run ID does not belong to the given experiment ID.")
-    
+    # client = MlflowClient()
+
+    # # Validate the run belongs to the experiment
+    # run = client.get_run(run_id)
+    # if run.info.experiment_id != experiment_id:
+    #     raise ValueError(
+    #         "The specified run ID does not belong to the given experiment ID."
+    #     )
 
     # Fetch all logged metrics for the run
     averages = {}
-    
+
     for metric_name in run.data.metrics.keys():
         if metric_name == "epoch":
             continue
 
-
-        # Get all metric history for the given metric name
-        metric_history = client.get_metric_history(run_id, metric_name)
-        
-
-        # Filter metrics by the specified step (step number)
-        step_metrics = [
-            metric.value for metric in metric_history
-            if metric.step == step_number
-        ]
-
-        if len(step_metrics) == 0:
-            print(f"E: No values found for metric '{metric_name}' at step {step_number}")
-        if len(step_metrics) > 1:
-            print(f"E: Multiple values found. {len(step_metrics)} values found for metric '{metric_name}' at step {step_number}")
-
-
-
-
         try:
-            if step_metrics:
-                averages[metric_name] = step_metrics[0]  # Just return the value for now
+            # Get all metric history for the given metric name
+            metric_history = client.get_metric_history(run.info.run_id, metric_name)
+            # Filter metrics by the specified step (step number)
+            step_metrics = [
+                metric.value for metric in metric_history if metric.step == step_number
+            ]
+
+            if len(step_metrics) == 0:
+                # print(
+                #    f"E: No values found for metric '{metric_name}' at step {step_number}"
+                # )
+                averages[metric_name] = (
+                    -1  # No values logged for this metric at the step
+                )
+            elif len(step_metrics) > 1:
+                print(
+                    f"E: Multiple values found. {len(step_metrics)} values found for metric '{metric_name}' at step {step_number}"
+                )
+                averages[metric_name] = step_metrics[
+                    0
+                ]  # Just return the first value for now
             else:
-                averages[metric_name] = None  # No values logged for this metric at the step
+                averages[metric_name] = step_metrics[0]  # Just return the value for now
+
         except Exception as e:
             print(f"E: Error computing average for metric '{metric_name}': {e}")
 
     return averages
+
+    # # Get all metric history for the given metric name
+    # metric_history = client.get_metric_history(run_id, metric_name)
+    # # Filter metrics by the specified step (step number)
+    # step_metrics = [
+    #     metric.value for metric in metric_history if metric.step == step_number
+    # ]
+
+    # if len(step_metrics) == 0:
+    #     print(
+    #         f"E: No values found for metric '{metric_name}' at step {step_number}"
+    #     )
+    # if len(step_metrics) > 1:
+    #     print(
+    #         f"E: Multiple values found. {len(step_metrics)} values found for metric '{metric_name}' at step {step_number}"
+    #     )
+
+    # try:
+    #     if step_metrics:
+    #         averages[metric_name] = step_metrics[0]  # Just return the value for now
+    #     else:
+    #         averages[metric_name] = (
+    #             None  # No values logged for this metric at the step
+    #         )
+    # except Exception as e:
+    #     print(f"E: Error computing average for metric '{metric_name}': {e}")
